8d374bb9238027b3971c4339f3456056eea7cffd
Alex J. Green <alexjeffreygreen@gmail.com>
2023-10-28 13:34:50 -0400

    Dialogue Window Init Commit
    Dialog Window initial commit with debug add and skip through for testing. Portraits are bound to the animation controller where the names of the characters will be their animation sprite frames.

--- Scripts/UI/Dialogue_Entry.gd
+++ Scripts/UI/Dialogue_Entry.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name DialogueEntry
 
-enum DIALOGUE_ACTOR {DOVER, JANE, CASEY, GEKO, SHELTON, CHOMPER, COBRA}
+enum DIALOGUE_ACTOR {Dover, Jane, Casey, Geko, Shelton, Chomper, Cobra}
 
 var Dialogue_Text : String
 var Dialogue_Actor : DIALOGUE_ACTOR
--- Scripts/UI/Dialogue_Window.gd
+++ Scripts/UI/Dialogue_Window.gd
@@ -1,18 +1,82 @@
 extends CanvasLayer
 class_name DialogeWindow
 
-@onready var dialogue_portrait = $DialoguePortrait
-@onready var dialogue_text = $DialogueText
-#@export var dialogue_actor  #TODO - Pass character so we know who's dialog it is
-
+@onready var dialogue_portrait : AnimatedSprite2D = $Dialogue_Portrait
+@onready var dialogue_text : Label = $Dialogue_Text
+var dialogue_cursor_text : String
+var current_dialogue : DialogueEntry
+var debug_str : String = "abcdefghijklmnopqrstuvwxyz ";
 var dialogues = []
-
+var skip_through : bool = false # not really needed
 
 func _ready():
-	pass
+	self.clear_dialogue_text()
 	
 func add_dialogue(entry : DialogueEntry):
-	self.dialogues.append(entry)
+	self.dialogues.push_back(entry)
 
 func remove_dialogue(entry : DialogueEntry):
-	self.dialogues.erase(entry)
+	var current_dialog = self.dialogues.pop_front()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("Confirm"):
+		self.debug_add_dialogue()
+		if self.current_dialogue == null:
+			self.current_dialogue = self.dialogues.pop_front()
+	if Input.is_action_just_pressed("Debug"):
+		#self.debug_add_dialogue()
+		self.clear_dialogue_text()
+		self.current_dialogue = self.dialogues.pop_front()
+		#self.current_dialogue = self.dialogues.pop_front()
+#	if Input.is_action_just_pressed("Confirm"):
+#		if self.current_dialogue != null:
+#			self.skip_through = true
+	self.display_dialogue()
+
+func display_dialogue():
+	if self.current_dialogue != null && (len(self.dialogue_cursor_text) < len(self.current_dialogue.Dialogue_Text) - 1):
+		if !self.dialogue_portrait.is_playing():
+			var dialogue_animation_str = DialogueEntry.DIALOGUE_ACTOR.keys()[self.current_dialogue.Dialogue_Actor]
+			self.dialogue_portrait.play(dialogue_animation_str)
+#		if self.skip_through == false:	
+		var currentLengthOfDialogue = len(self.dialogue_cursor_text)
+		var nextStringValue = self.current_dialogue.Dialogue_Text[currentLengthOfDialogue + 1]
+		self.dialogue_cursor_text += nextStringValue
+#		else:
+#			self.skip_through = false
+#			self.dialogue_cursor_text = ""
+#			self.dialogue_cursor_text = self.current_dialogue.Dialogue_Text
+		
+		self.dialogue_text.text = self.dialogue_cursor_text
+		
+	if self.current_dialogue == null:
+		self.dialogue_portrait.stop() # Stop Animation
+		self.dialogue_portrait.frame = 0 #Reset the Animation
+		self.dialogue_portrait.visible = false
+	else:
+		self.dialogue_portrait.visible = true
+
+func random_string(length : int):
+	var word: String
+	var n_char = len(self.debug_str)
+	for i in range(length):
+		word += self.debug_str[randi() % n_char]
+	return word
+func debug_add_dialogue():
+	var new_message = random_string(randi_range(1, self.debug_str.length()))
+	var dialogue_entry = DialogueEntry.new()
+	dialogue_entry.Dialogue_Actor = DialogueEntry.DIALOGUE_ACTOR.Dover
+	dialogue_entry.Dialogue_Portrait = null #TODO
+	dialogue_entry.Dialogue_Text = new_message
+	self.add_dialogue(dialogue_entry)
+
+func clear_dialogue_text():
+	self.dialogue_text.text = ""
+	self.dialogue_cursor_text = ""
+
+#func _on_typewriter_timer_timeout():
+#	if self.current_dialogue != null && len(self.dialogue_cursor_text) < len(self.current_dialogue.Dialogue_Text) - 1:
+#		var currentLengthOfDialogue = len(self.dialogue_cursor_text)
+#		var nextStringValue = self.current_dialogue.Dialogue_Text[currentLengthOfDialogue + 1]
+#		self.dialogue_cursor_text += nextStringValue
+#		self.dialogue_text.text = self.dialogue_cursor_text
